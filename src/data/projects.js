0cd42a4f15bef61008be0775b01f27c194890eed
prajina-Adhikari <aprajina45@gmail.com>
2026-09-03 15:01:56 +0545

    aaded projects

---
+++ src/data/projects.js
@@ -0,0 +1,51 @@
+export const projects = [
+  {
+    title: "School Management System",
+    description:
+      "A full-stack school management platform for managing students, teachers, attendance, courses, notices and other academic activities.",
+    technologies: [
+      "React",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "Tailwind CSS",
+    ],
+    github:
+      "https://github.com/prajina8/Smart-school",
+    demo: "#",
+    category: "Full Stack",
+  },
+
+  {
+    title: "Expense Tracker",
+    description:
+      "A personal expense tracking application that allows users to record, manage and visualize their income and expenses.",
+    technologies: [
+      "React",
+      "JavaScript",
+      "Chart.js",
+      "CSS",
+    ],
+    github:
+      "https://github.com/prajina8/Expense-Tracker",
+    demo: "#",
+    category: "Full-Stack",
+  },
+
+  {
+    title: "Job & Internship Portal",
+    description:
+      "A MERN-based platform connecting applicants and recruiters with job search, applications, profiles and job recommendations.",
+    technologies: [
+      "React",
+      "Node.js",
+      "Express",
+      "MongoDB",
+    ],
+    github: "https://github.com/prajina8/JobRadar",
+    demo: "#",
+    category: "Full Stack",
+  },
+
+ 
+];
