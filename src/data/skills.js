5e2d53dba3ceaa15028e638af2db31ab77e26527
prajina-Adhikari <aprajina45@gmail.com>
2026-09-03 14:59:23 +0545

    aaded skills

---
+++ src/data/skills.js
@@ -0,0 +1,52 @@
+export const skills = [
+  {
+    name: "React",
+    level: "Advanced",
+    category: "Frontend",
+  },
+  {
+    name: "JavaScript",
+    level: "Advanced",
+    category: "Frontend",
+  },
+  {
+    name: "HTML & CSS",
+    level: "Advanced",
+    category: "Frontend",
+  },
+  {
+    name: "Tailwind CSS",
+    level: "Advanced",
+    category: "Frontend",
+  },
+  {
+    name: "Node.js",
+    level: "Intermediate",
+    category: "Backend",
+  },
+  {
+    name: "Express.js",
+    level: "Intermediate",
+    category: "Backend",
+  },
+  {
+    name: "MongoDB",
+    level: "Intermediate",
+    category: "Database",
+  },
+  {
+    name: "Git & GitHub",
+    level: "Intermediate",
+    category: "Tools",
+  },
+  {
+    name: "REST API",
+    level: "Intermediate",
+    category: "Backend",
+  },
+  {
+    name: "C / C++",
+    level: "Intermediate",
+    category: "Programming",
+  },
+];
